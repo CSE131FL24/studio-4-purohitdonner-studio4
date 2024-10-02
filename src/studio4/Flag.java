ee3cc83aad81efe9a6376a922efcf2fbea1ab9d9
shivampurohit <shivampurohit@localhost>
2024-10-02 14:08:33 -0500

    Studio 4 Complete

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,20 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.clear();
+		StdDraw.setXscale(1, 22);
+		StdDraw.setYscale(10, 16);
+		StdDraw.setPenColor(254, 123, 12);
+		StdDraw.filledRectangle(3, 3, 3, 3);
+		StdDraw.filledRectangle(3, 13, 3, 3);
+		StdDraw.filledRectangle(16, 3, 6, 3);
+		StdDraw.filledRectangle(16, 13, 6, 3);
+		StdDraw.setPenColor(1, 32, 126);
+		StdDraw.filledRectangle(11, 8, 11, 1);
+		StdDraw.filledRectangle(8, 8, 1, 8);
+		StdDraw.setPenColor(1, 1, 1);
+		StdDraw.filledRectangle(3, 2, 1, 1);
+		StdDraw.filledRectangle(0, 1, 1, 3);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,8 @@
 package studio4;
 
 import java.io.File;
+import java.awt.Color;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -19,6 +21,19 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shapetype = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		Color mycolor = new Color(redComponent, greenComponent, blueComponent);
+		if(shapetype == "ellipse") {
+			StdDraw.setPenColor(mycolor);
+			StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}
 	}
 }
